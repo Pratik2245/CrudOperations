776ff70175a4dfb399c8dc2ea1ebe574b12b76e0
Pratik2245 <paithankarpratik3@gmail.com>
2024-01-13 14:23:54 +0530

    000webhost

--- app/src/main/java/com/example/crudoperations/linkApi.java
+++ app/src/main/java/com/example/crudoperations/linkApi.java
@@ -1,5 +1,6 @@
 package com.example.crudoperations;
 
 public class linkApi {
-    static String url="http://192.168.190.72/example_db_api/";
+//    static String url="http://192.168.190.72/example_db_api/";
+    static  String url="http://sanjivanimentor.000webhostapp.com/SanjivaniMentor/";
 }
